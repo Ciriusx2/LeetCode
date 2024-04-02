3a6441239fc9cbf80a230e4e2cd9ac6c65c70f8d
Dhruv <102383403+Ciriusx2@users.noreply.github.com>
2024-04-02 14:37:02 +0530

    Time: 40 ms (15.62%), Space: 16.6 MB (8.94%) - LeetHub

--- 3099-harshad-number/3099-harshad-number.py
+++ 3099-harshad-number/3099-harshad-number.py
@@ -1,4 +1,10 @@
 class Solution:
     def sumOfTheDigitsOfHarshadNumber(self, x: int) -> int:
-        smDigits = sum(map(int,str(x)))
-        return smDigits if x % smDigits == 0 else -1
+        s = 0
+        original = x
+        while x > 0:
+            s += x % 10
+            x //=10
+        if original % s == 0:
+            return s
+        return -1
