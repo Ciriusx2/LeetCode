9a72135369e5100fd7d19649ccf9b4011163dad4
Dhruv <102383403+Ciriusx2@users.noreply.github.com>
2024-04-03 23:25:25 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (74.16%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,46 +1,56 @@
-import java.util.List;
-
 class Solution {
-    int l, m, n;
-    int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-    
-    public boolean find(char[][] board, int i, int j, String word, int idx) {
-        if (idx >= l)
-            return true;
-        
-        if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != word.charAt(idx))
-            return false;
-        
-        char temp = board[i][j];
-        board[i][j] = '$';
-        
-        for (int[] dir : directions) {
-            int i_ = i + dir[0];
-            int j_ = j + dir[1];
-            
-            if (find(board, i_, j_, word, idx + 1))
-                return true;
-        }
-        
-        board[i][j] = temp;
-        return false;
-    }
-    
     public boolean exist(char[][] board, String word) {
-        m = board.length;
-        n = board[0].length;
-        l = word.length();
-        if (m * n < l)
+        int n = board.length, m = board[0].length;
+        boolean[][] visited = new boolean[n][m];
+        char[] wordChar = word.toCharArray();
+        if (wordChar.length > n * m)
             return false;
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == word.charAt(0) && find(board, i, j, word, 0)) {
-                    return true;
-                }
+        int counts[] = new int[256];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                counts[board[i][j]]++;
             }
         }
-        
+        int len = wordChar.length;
+        for(int i=0; i<len/2; i++)
+        {
+            if(counts[wordChar[i]]>counts[wordChar[len-1-i]]){
+                for(int j=0; j<len/2; j++)
+                {
+                    char temp = wordChar[j];
+                    wordChar[j]=wordChar[len-1-j];
+                    wordChar[len-1-j]=temp;
+                }
+                break;
+            }
+        }
+        for (char c : wordChar) {
+            if (--counts[c] < 0)
+                return false;
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (visit(board, wordChar, 0, i, j, n, m, visited))
+                    return true;
+            }
+        }
         return false;
+    }
+
+    private boolean visit(char[][] board, char[] word, int start, int x, int y,
+            int n, int m, boolean[][] visited) {
+        if (start == word.length)
+            return true;
+        if (x < 0 || x >= n || y < 0 || y >= m || visited[x][y])
+            return false;
+        if (word[start] != board[x][y])
+            return false;
+        visited[x][y] = true;
+        boolean found = visit(board, word, start + 1, x + 1, y, n, m, visited)
+                || visit(board, word, start + 1, x - 1, y, n, m, visited)
+                || visit(board, word, start + 1, x, y + 1, n, m, visited)
+                || visit(board, word, start + 1, x, y - 1, n, m, visited);
+        visited[x][y] = false;
+        return found;
     }
 }
