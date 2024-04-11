c172081943739be5626eccaa5607730697bb771b
Dhruv <102383403+Ciriusx2@users.noreply.github.com>
2024-04-11 22:50:28 +0530

    Time: 2 ms (100.00%), Space: 44.4 MB (96.18%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.java
+++ 0402-remove-k-digits/0402-remove-k-digits.java
@@ -1,32 +1,29 @@
 class Solution {
     public String removeKdigits(String num, int k) {
-        Stack<Character> stack = new Stack<>();
-        
-        for (char digit : num.toCharArray()) {
-            while (!stack.isEmpty() && k > 0 && stack.peek() > digit) {
-                stack.pop();
-                k--;
-            }
-            stack.push(digit);
-        }
-        
-        while (k > 0 && !stack.isEmpty()) {
-            stack.pop();
-            k--;
-        }
-        
-        StringBuilder sb = new StringBuilder();
-        while (!stack.isEmpty()) {
-            sb.append(stack.pop());
-        }
-        sb.reverse(); 
-        
-        
-        while (sb.length() > 0 && sb.charAt(0) == '0') {
-            sb.deleteCharAt(0);
-        }
-        
-        
-        return sb.length() > 0 ? sb.toString() : "0";
+       	char[] digits = num.toCharArray();
+		if (k == num.length()) {
+			return "0";
+		}
+
+		char[] stack = new char[digits.length];
+		int stackTop = -1;
+		int removalCount = k;
+
+		for (int i = 0; i < digits.length; i++) {
+			while (removalCount > 0 && stackTop >= 0 && stack[stackTop] > digits[i]) {
+				stackTop--;
+				removalCount--;
+			}
+			stackTop++;
+			stack[stackTop] = digits[i];
+		}
+
+		int nonZeroStart = 0;
+
+		while (stack[nonZeroStart] == '0' && nonZeroStart < digits.length - k - 1) {
+			nonZeroStart++;
+		}
+
+		return String.valueOf(stack, nonZeroStart, digits.length - k - nonZeroStart);
     }
 }
