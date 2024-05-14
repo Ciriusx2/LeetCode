33361cd176605072004a6bedd81f19a12f002be5
Dhruv <102383403+Ciriusx2@users.noreply.github.com>
2024-05-14 12:57:45 +0530

    Time: 12 ms (99.54%), Space: 41 MB (56.84%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.java
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.java
@@ -1,36 +1,58 @@
 class Solution {
-    int[] roww = {1, -1, 0, 0};
-    int[] coll = {0, 0, -1, 1};
-    int maxGold = 0;
-
-    public int dfs(int[][] grid, int x, int y, int n, int m) {
-        if (x < 0 || x >= n || y < 0 || y >= m || grid[x][y] == 0) return 0;
-        
-        int curr = grid[x][y];
-        grid[x][y] = 0;
-        int localMaxGold = curr;
-
-        for (int i = 0; i < 4; i++) {
-            int newX = x + roww[i];
-            int newY = y + coll[i];
-            localMaxGold = Math.max(localMaxGold, curr + dfs(grid, newX, newY, n, m));
+    public int max = 0;
+    public void ways(int[][] grid,int r, int c, int gold){
+        if(gold > max){
+            max = gold;
         }
+        if(grid[r][c] == 0){
+            return;
+        }
+        int a = grid[r][c];
+        grid[r][c] = 0;
+        if(r - 1 >= 0){
+            ways(grid,r - 1, c, gold + a);
+        }
+        if(r + 1 <= grid.length - 1){
+            ways(grid,r + 1,c,gold + a);
+        }
+        if(c + 1 <= grid[0].length - 1){
+            ways(grid,r, c + 1, gold + a);
+        }
+        if(c - 1 >= 0){
+            ways(grid,r, c - 1, gold + a);
+        }
+        grid[r][c] = a;
+        return ;
 
-        grid[x][y] = curr;
-        return localMaxGold;
     }
 
-    public int getMaximumGold(int[][] grid) {
-        int n = grid.length, m = grid[0].length;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] != 0) {
-                    maxGold = Math.max(maxGold, dfs(grid, i, j, n, m));
+    public int gridWithNoZeros(int[][] grid){
+        int count = 0;
+        for(int i = 0; i < grid.length; i++){
+            for (int j = 0; j < grid[0].length; j++){
+                if(grid[i][j] == 0){
+                    return -1;
                 }
+                else
+                    count += grid[i][j];
             }
         }
+        return count;
+    }  
 
-        return maxGold;
+
+
+    public int getMaximumGold(int[][] grid) {
+
+        int count = gridWithNoZeros(grid);
+        if(count != -1) return count;
+
+        
+        for(int i = 0; i < grid.length; i ++){
+            for(int j = 0 ; j < grid[0].length ; j++){
+                ways(grid,i,j,0);
+            }
+        }
+        return max;
     }
 }
