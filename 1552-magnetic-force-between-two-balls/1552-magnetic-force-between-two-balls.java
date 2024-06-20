718d98de8770a03a4beb86284845a828e51e529e
Dhruv <102383403+Ciriusx2@users.noreply.github.com>
2024-06-20 11:32:42 +0530

    Time: 35 ms (99.46%), Space: 55.8 MB (37.12%) - LeetHub

--- 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.java
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.java
@@ -1,40 +1,34 @@
 class Solution {
     public int maxDistance(int[] position, int m) {
         Arrays.sort(position);
-        int lo = 1;
-        int hi = (position[position.length-1]-position[0])/(m-1);
-        int ans = 1;
-        while(lo<=hi)
-        {
-            int mid = lo+(hi-lo)/2;
-            if(canWePlace(position, mid, m))
-            {
-                ans = mid;
-                lo=mid+1;
-            }
-            else
-            {
-                hi = mid-1;
-            }
-        }
-        return ans;
+        
+        
+             int start=1;
+             int end=(position[position.length-1]-position[0])/(m-1);
+             while(start<=end){
+                 int mid=(end+start)/2;
+                   int req=dist(position,mid);
+                   if(req>=m){
+                    start=mid+1;
+                   }
+                   else{
+                    end=mid-1;
+                   }
+
+             
+             }
+             return end;
+
     }
-    private boolean canWePlace(int[] arr, int dist, int cows)
-    {
-        int cntCows=1;
-        int last = arr[0];
-        for(int i=0;i<arr.length;i++)
-        {
-            if(arr[i]-last>=dist)
-            {
-                cntCows++;
-                last = arr[i];
-            }
-            if(cntCows>=cows)
-            {
-                return true;
-            }
+    public int dist(int[] nums,int mid){
+        int count=1;
+       int last=nums[0];
+        for(int i=1;i<nums.length;i++){
+             if(nums[i]-last>=mid){
+                count++;
+                last=nums[i];
+             }
         }
-        return false;
+        return count;
     }
 }
