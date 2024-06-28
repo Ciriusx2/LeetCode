3a543847aab9a78d8c7d1ee1855cdc734c5c4784
Dhruv <102383403+Ciriusx2@users.noreply.github.com>
2024-06-28 10:49:15 +0530

    Time: 4 ms (99.46%), Space: 63.3 MB (44.87%) - LeetHub

--- 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.java
+++ 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.java
@@ -1,31 +1,21 @@
 class Solution {
     public long maximumImportance(int n, int[][] roads) {
-        // Array to store the degree of each city
-        int[] degree = new int[n];
-        
-        // Calculate the degree of each city
-        for (int[] road : roads) {
-            degree[road[0]]++;
-            degree[road[1]]++;
+        int[] br = new int[n];
+        for(int[] r : roads){
+            br[r[0]]++;
+            br[r[1]]++;
         }
-        
-        // Create a list of cities and sort by degree
-        Integer[] cities = new Integer[n];
-        for (int i = 0; i < n; i++) {
-            cities[i] = i;
+        int[] cnt = new int[n];
+        for(int b : br){
+            cnt[b]++;
         }
-        Arrays.sort(cities, new Comparator<Integer>() {
-            public int compare(Integer a, Integer b) {
-                return Integer.compare(degree[b], degree[a]);
+        long sum = 0;
+        long val = 1;
+        for(long i = 0; i < n; i++){
+            for(int j = 0; j < cnt[(int)i]; j++){
+                sum += i*val++;
             }
-        });
-        
-        // Assign values to cities starting from the highest degree
-        long totalImportance = 0;
-        for (int i = 0; i < n; i++) {
-            totalImportance += (long) (n - i) * degree[cities[i]];
         }
-        
-        return totalImportance;
+        return sum;
     }
 }
