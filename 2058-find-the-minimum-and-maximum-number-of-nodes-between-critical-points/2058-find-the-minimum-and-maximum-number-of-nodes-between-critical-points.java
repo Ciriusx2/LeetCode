4dc615d5b73e78908473f442c7737354254e1001
Dhruv <102383403+Ciriusx2@users.noreply.github.com>
2024-07-05 10:51:34 +0530

    Time: 4 ms (100.00%), Space: 60.5 MB (64.65%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -1,38 +1,59 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public int[] nodesBetweenCriticalPoints(ListNode head) {
-        ListNode pre = head;
-        ListNode cur = head.next;
-        int[] ans = {-1, -1};
-        int prePosition = -1, curPosition = -1, firstPosition = -1, position = 0;
-        while (cur.next != null) {
-            if ((cur.val < pre.val && cur.val < cur.next.val) || (cur.val > pre.val && cur.val > cur.next.val)) {
-                // local
-                prePosition = curPosition;
-                curPosition = position;
-                if (firstPosition == -1) {
-                    firstPosition = position;
-                }
-                if (prePosition != -1) {
-                    if (ans[0] == -1) ans[0] = curPosition - prePosition;
-                    else ans[0] = Math.min(ans[0], curPosition - prePosition);
-                    ans[1] = position - firstPosition;
-                }
+
+       ListNode prev=head;
+       ListNode curr=head.next;
+
+       int first_index=-1;
+       int last_index=-1;
+
+       int index=1;
+
+       int prev_index=-1;
+
+       int min_dist=Integer.MAX_VALUE;
+
+       while(curr.next!=null){
+
+        if(prev.val>curr.val && curr.val<curr.next.val   || prev.val<curr.val && curr.val>curr.next.val){
+
+            if(prev_index==-1){
+                first_index=index;
+                prev_index=index;
             }
-            
-            position++;
-            pre = cur;
-            cur = cur.next;
+            else{
+                if(min_dist>index-prev_index){
+                    min_dist=index-prev_index;
+                }
+
+                prev_index=index;
+            }
+
         }
-        return ans;
+
+        index++;
+
+
+        curr=curr.next;
+        prev=prev.next;
+
+       }
+
+       last_index=prev_index;
+
+       int max_dist=-1;
+
+      
+
+       if(min_dist==Integer.MAX_VALUE ){
+         int[]arr={-1,-1};
+         return arr;
+       }
+       else{
+        max_dist=last_index-first_index;
+        int []arr={min_dist,max_dist};
+        return arr;
+       }
+
     }
 }
